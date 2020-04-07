5bfcc793daac2b13b061c0aaed7ad5d2ceb73868
adharamonzon <adharamonzon@gmail.com>
2020-04-07 08:34:06 +0200

    Update docs

---
+++ docs/precache-manifest.c69bbec3a8e8be6b7a232ffb0331e243.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e506fa49f0c00a684964c6eb2d54b91",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0a377e30631ea4de73e8",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e9f04c32ce925b83bc5d",
+    "url": "./static/js/2.e615a12e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.e615a12e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a377e30631ea4de73e8",
+    "url": "./static/js/main.9d31fa91.chunk.js"
+  },
+  {
+    "revision": "8e0dab9134146f67ba4f",
+    "url": "./static/js/runtime-main.0f7f4da8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
